10b661324e309cb65b7727df8014417a2e516eb3
Yanis540 <yanistabellout4@gmail.com>
2024-09-30 11:37:39 +0200

    adding frontend

---
+++ DAAR/TP/TP2/frontend/db/migrations/1607794093392-create-books-table.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS moddatetime;
+-- Your migration code here.create table books (
+create table books (
+  id uuid primary key default uuid_generate_v4(),
+  info json,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp
+);
+
+create trigger books_moddatetime
+  before update on books
+  for each row
+  execute procedure moddatetime (updated_at);
