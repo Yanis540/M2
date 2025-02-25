7ed8f47f09520e54a51abdc5216d4d6708080b48
Yanis540 <yanistabellout4@gmail.com>
2025-02-25 23:32:16 +0100

    added stuff

---
+++ AAGA/notes.ts
@@ -0,0 +1,9 @@
+/*
+
+! SMA (Système Multi-Agents): 
+?   il existe une mémoire partagé 
+?   pas bcp de ressources
+! MA (modèle acteur) : 
+?   il existe une mémoire partagé agent bcp de ressources 
+?   Event bus (r/w auomtiatuqye)
+*/
